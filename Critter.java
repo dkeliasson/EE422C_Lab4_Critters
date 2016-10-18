859dfdbc587108a6902564ab2c7c71b00a1e8a7d
alexut27 <ashvedov@utexas.edu>
2016-10-18 00:29:45 -0500

    Update Critter.java
    Committed the worldTimeStep method

--- Critter.java
+++ Critter.java
@@ -40,7 +40,180 @@ public abstract class Critter {
 	public static void setSeed(long new_seed) {
 		rand = new java.util.Random(new_seed);
 	}
-	
+		public static void worldTimeStep() {
+		/* Iterate through every critter and invoke the doTimeStep method */
+		Critter critter;
+		int i = 0;
+		while(i < population.size() - 1){
+			critter = population.get(i);
+			critter.doTimeStep();
+			i++;
+		}
+		
+		/* Resolve encounters between critters */
+		Critter a;
+		Critter b;
+		for(int row = 0; row < Params.world_height; row++){
+			for(int col = 0; col < Params.world_width; col++){
+
+					/* More than 1 critter in coordinate */
+					/* We must battle */
+					/* Pull out 2 critters for battle(hopefully) */
+					a = (Critter)ourworld[row][col].get(0);
+					b = (Critter)ourworld[row][col].get(1);
+					
+					
+					
+					/* Resolve any conflict */
+					/* FIGHT TIME */
+					/* LETS ROCK THIS */
+					/* Check first if either has moved to keep track of in case one attempts to run */
+					boolean Amoved = a.hasMoved;
+					boolean Bmoved = b.hasMoved;
+					
+					/* Hold each critters energy to replace if they come back to fight */
+					int Aenergy = a.energy;
+					int Benergy = b.energy;
+					
+					/* Now we fight? */
+					boolean fight_choiceA = a.fight(b.toString());
+					boolean fight_choiceB = b.fight(a.toString());
+					
+					/* Both chose to duke it out */
+					if(fight_choiceA && fight_choiceB){
+						int rollA = Critter.getRandomInt(a.energy);
+						int rollB = Critter.getRandomInt(b.energy);
+						if(rollA >= rollB){
+							if(a.energy <= 0){}
+							else{
+								a.energy += (b.energy/2);
+								b.energy = 0;
+								ourworld[row][col].remove(1);
+							}
+						}
+						else{
+							b.energy += (a.energy/2);
+							a.energy = 0;
+							ourworld[row][col].remove(0);
+						}
+					}
+					/* B wants to run away */
+					else if(fight_choiceA && !fight_choiceB){
+						/* if b moved prior to fight then no choice but to fight */
+						if(Bmoved){
+							a.energy += (b.energy/2);
+							b.energy = 0;
+							ourworld[row][col].remove(1);
+						}
+						/* else if b moved during call to fight check that it didn't move to occupied place */
+						else if(!Bmoved && b.hasMoved){
+							/* Check to see if place that b moved to is populated already */
+							boolean populated = false;
+							for(i = 0; i < population.size(); i++){
+								if((b.x_coord == population.get(i).x_coord) && (b.y_coord == population.get(i).y_coord)){
+									populated = true;
+								}
+							}
+							/* if b moved to populated coordinate then they fight */
+							if(populated){
+								a.energy += (Benergy/2);
+								b.energy = 0;
+								ourworld[row][col].remove(1);
+							}
+							else{}
+						}					
+					}
+					/* A wants to run away */
+					else if(!fight_choiceA && fight_choiceB){
+						/* if a moved prior to fight then no choice but to fight */
+						if(Amoved){
+							int rollA = Critter.getRandomInt(a.energy);
+							int rollB = Critter.getRandomInt(b.energy);
+							if(rollA >= rollB){
+								if(a.energy <= 0){}
+								else{
+									a.energy += (b.energy/2);
+									b.energy = 0;
+									ourworld[row][col].remove(1);
+								}
+							}
+							else{
+								b.energy += (a.energy/2);
+								a.energy = 0;
+								ourworld[row][col].remove(0);
+							}
+						}
+						/* else if a moved during call to fight check that it didn't move to occupied place */
+						else if(!Amoved && a.hasMoved){
+							/* Check to see if place that b moved to is populated already */
+							boolean populated = false;
+							for(i = 0; i < population.size(); i++){
+								if((a.x_coord == population.get(i).x_coord) && (a.y_coord == population.get(i).y_coord)){
+									populated = true;
+								}
+							}
+							/* if a moved to populated coordinate then they fight */
+							if(populated){
+								int rollA = Critter.getRandomInt(a.energy);
+								int rollB = Critter.getRandomInt(b.energy);
+								if(rollA >= rollB){
+									a.energy = Aenergy;
+									a.energy += (b.energy/2);
+									b.energy = 0;
+									ourworld[row][col].remove(1);
+								}
+								else{
+									b.energy += (a.energy/2);
+									a.energy = 0;
+									ourworld[row][col].remove(0);
+								}
+							}
+							else{}
+						}		
+					}
+					/* else they both are babies and want to run away crying */
+					else{
+						/* Check all possible combinations of A and B movements */
+						
+					}		
+				}	
+				/* Refresh Algae */
+				int count = Params.refresh_algae_count;
+				while(count != 0){
+					critter = new Algae();
+					critter.energy = Params.start_energy;
+					/* Assign a random position */
+					critter.x_coord = getRandomInt(Params.world_width - 1);
+					critter.y_coord = getRandomInt(Params.world_height - 1);
+					/* Add this guy to the population */
+					population.add(critter);
+				}
+					
+					
+				/* Add any new offspring to collection */
+				for(int j = 0; j < babies.size(); j++){
+					population.add(babies.get(j));
+					babies.remove(j);
+				}
+				
+				/* Deduct rest energy */
+				for(int m = 0; m < population.size(); m++){
+					population.get(m).energy -= Params.rest_energy_cost;
+				}
+				
+				/* Clear all dead critters */
+				for(int k = 0; k < population.size(); k++){
+					if(population.get(k).energy <= 0){
+						population.remove(k);
+					}
+				}
+				
+				/* Set all critters move field back to false */
+				for(int l = 0; l < population.size(); l++){
+					population.get(l).hasMoved = false;
+				}
+			}
+		}
 	
 	/* a one-character long string that visually depicts your critter in the ASCII interface */
 	public String toString() { return ""; }
